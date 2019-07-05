e4516de9aa22fc8c2e30028be10e52e334d2d143
Timothy Condon <151268@cityhall.boston.cob>
2019-07-05 09:05:29 -0400

    Fix up geocoding for thankavet

--- geocode_organize.R
+++ geocode_organize.R
@@ -6,8 +6,8 @@ library(xlsx)
 gs_auth(new_user = TRUE)
 
 # Check available sheets
-gs_ls()
-available <- gs_ls()
+#gs_ls()
+#available <- gs_ls()
 
 # Read in the art extracted spreadsheet
 tav <- gs_title("3_21_2019 NEW _Veteran Xtract_")
@@ -16,13 +16,23 @@ tav <- gs_read(tav)
 thank <- tav
 thank$geoaddress <- NA
 
+thank$`Mailing Address - Apartment Number` [ is.na(thank$`Mailing Address - Apartment Number`) ] <- ''
+
 thank <- unite(thank, geoaddress, c('Residential Address - Street Number',
                            'Residential Address - Street Name',
-                           'Mailing Address - Apartment Number',
                            'Residential Address - Zip Code'),
       sep = ' ', remove = FALSE)
 
 write.csv(thank, 'tables/tav_geordy.csv', row.names = FALSE)
 
+thank2 <- tav
+thank2$geoaddress <- NA
 
+thank2$`Mailing Address - Apartment Number` [ is.na(thank2$`Mailing Address - Apartment Number`) ] <- ''
 
+thank2 <- unite(thank2, geoaddress, c('Residential Address - Street Number',
+                                    'Residential Address - Street Name',
+                                    'Residential Address - Zip Code'),
+               sep = ' ', remove = FALSE)
+
+write.csv(thank2, 'tables/tav_geordy_2.csv', row.names = FALSE)
